5e43fa8d82952bcde5ea993d33168596cfbd6162
JohnYezub <buze@ya.ru>
2020-08-18 12:25:34 +0800

    Initial Commit

---
+++ CustomSwitcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSwitcher
+//
+//  Created by   admin on 18.08.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
